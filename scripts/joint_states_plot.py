414bc9fe36e6541fe38e21324064bbb218d5f1a1
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2014-11-22 19:41:49 +0100

    Trying to add more plots, not really working, also adding some ideas on how to generate the full visualization automatically

--- scripts/joint_states_plot.py
+++ scripts/joint_states_plot.py
@@ -4,6 +4,9 @@
 Created on 22/11/14
 
 @author: Sam Pfeiffer
+
+Script that tries to show all joints of joint states
+in a bokeh web plot
 """
 
 import rospy
@@ -20,23 +23,48 @@ class JointStatesPlotter():
     def __init__(self):
         # Some needed vars
         rospy.loginfo("Initializing JointStatesPlotter")
-        self.max_points = 1000
+        self.max_points = 999999999#1000
         self.last_js = None
         self.x_data = []
         self.y_data = []
+        
+        # TODO: Get one message and get all joint names
+        # For every joint name get its upper and lower limit
+        # For every joint create a plot with its name
+        
         rospy.Subscriber(JS_TOPIC, JointState, self.js_cb)
         output_server("joint_states_continuous")
         
         figure(title="Line example for joint states")
-        line(self.x_data, self.y_data, color="#0000FF", x_axis_type = "datetime", 
+        line(self.x_data, self.y_data, 
+            self.get_joint_limits('arm_left_1_joint'),
+            color="#0000FF", x_axis_type = "datetime", 
             tools="pan,resize,wheel_zoom", width=1200,height=300, 
             title = 'Joint states for arm_left_1_joint',
             legend='joint value')
-         
         xaxis()[0].axis_label = "Time"
         yaxis()[0].axis_label = "Joint pos"
+        # Testing random stuff to get more plots to work
+#         p1 = curplot()
+#         figure(title="Line example for joint states")
+#         line(self.x_data, self.y_data, color="#0000FF", x_axis_type = "datetime", 
+#             tools="pan,resize,wheel_zoom", width=1200,height=300, 
+#             title = 'Joint states for arm_left_2_joint',
+#             legend='joint value')
+#         xaxis()[0].axis_label = "Time"
+#         yaxis()[0].axis_label = "Joint pos"
+#         p2 = curplot()
+#         gp = GridPlot(children=[[p1,p2]])
+#         show(gp)
         show()
         rospy.loginfo("Plotted!")
+
+    def get_joint_limits(self, joint_name):
+        lower_limit = -4.0
+        upper_limit = 4.0
+        # Do magic to get limits with urdf_parser_py
+        return lower_limit, upper_limit
+
 
     def js_cb(self, data):
         rospy.logdebug("Got CB of JS")
@@ -51,6 +79,7 @@ class JointStatesPlotter():
             self.y_data.append(data.position[0])
         
     def run_example(self):
+        """Initial test that just does one plot with some values of joint states"""
         rospy.loginfo("Plotting!")
         js_msgs = []
         # Get 10 joint state messages
@@ -69,7 +98,7 @@ class JointStatesPlotter():
         output_server("joint_states")
         
         figure(title="Line example for joint states")
-        line(x_data, y_data, color="#0000FF", x_axis_type = "datetime", 
+        line(x_data, y_data, y_range=[-1.0, 3.0], color="#0000FF", x_axis_type = "datetime", 
             tools="pan,resize", width=1200,height=300, 
             title = 'Joint states for arm_left_1_joint',
             legend='joint value')
@@ -80,16 +109,23 @@ class JointStatesPlotter():
         rospy.loginfo("Plotted!")
         
     def run_continuous(self):
-        renderer = [r for r in curplot().renderers if isinstance(r, Glyph)][0]
+        #renderers = [r for r in curplot().renderers if isinstance(r, Glyph)]
+        renderers = curplot().select(dict(type=Glyph))
         print "Renderer is:"
-        print renderer
-        datasource = renderer.data_source
+        print renderers
+        print len(renderers)
+        datasource = renderers[0].data_source
+        #datasource2 = renderers[1].data_source
         while not rospy.is_shutdown():
             #rospy.logwarn("x_data: " + str(self.x_data))
             datasource.data["x"] = self.x_data
             datasource.data["y"] = self.y_data
             datasource._dirty = True
             cursession().store_objects(datasource)
+#             datasource2.data["x"] = self.x_data / 10.0
+#             datasource2.data["y"] = self.y_data / 10.0
+#             datasource2._dirty = True
+#             cursession().store_objects(datasource2)
             rospy.sleep(0.01)
         
 
