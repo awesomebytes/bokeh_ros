cef2960788ba8d76229bdc77970fd433976aadd4
Sam Pfeiffer <sammypfeiffer@gmail.com>
2014-11-23 00:54:45 +0100

    Updated a version that works, using latest bokeh version

--- scripts/multiple_updateable_plots.py
+++ scripts/multiple_updateable_plots.py
@@ -8,11 +8,13 @@ Created on 22/11/14
 """
 
 from bokeh.plotting import *
-from bokeh.objects import Glyph
 import random
 import time
+from bokeh.models.renderers import GlyphRenderer
 
+MAX_DATA = 20
 
+# We will store the data here
 x_data = []
 y_data = []
 
@@ -21,42 +23,49 @@ y_data2 = []
 
 output_server("multiple_updateable_plots")
 
-figure(title="First plot")
-line(x_data, y_data,
+# Set up first plot
+p1 = figure(title="First plot")
+p1.line(x_data, y_data,
     color="#0000FF",
-    tools="pan,resize,wheel_zoom", width=1200,height=300, 
-    title = 'First plot stuff',
+    tools="pan,resize,wheel_zoom", width=1200,height=300,
     legend='value of thing')
-xaxis()[0].axis_label = "Time"
-yaxis()[0].axis_label = "Value"
 
+# Set up second plot
+p2 = figure(title="Second plot")
+p2.line(x_data2, y_data2, 
+    color="#00FFFF",
+    tools="pan,resize,wheel_zoom", width=1200,height=300,
+    legend='value of thing')
 
-figure(title="Second plot")
-line(x_data2, y_data2, 
-    color="#0000FF",
-    tools="pan,resize,wheel_zoom", width=1200,height=300, 
-    title = 'Second plot stuff',
-    legend='value of thing2')
-xaxis()[0].axis_label = "Time"
-yaxis()[0].axis_label = "Value"
-
+# Show plots!
 show()
 
-
-renderers = curplot().select(dict(type=Glyph))
-datasource = renderers[0].data_source
-time_ref = 0
+# Set up the dynamic plotting
+renderer1 = p1.select(dict(type=GlyphRenderer))
+renderer2 = p2.select(dict(type=GlyphRenderer))
+ds1 = renderer1[0].data_source
+ds2 = renderer2[0].data_source
 while True:
-    datasource.data["x"] = x_data
-    datasource.data["y"] = y_data
-    datasource._dirty = True
-    cursession().store_objects(datasource)
-    # How to update data of each plot?
-    # This updates the last one only
+    time_ref = time.time() % 60
+    ds1.data["x"] = x_data
+    ds1.data["y"] = y_data
+    ds1._dirty = True
+    cursession().store_objects(ds1)
+    ds2.data["x"] = x_data2
+    ds2.data["y"] = y_data2
+    ds2._dirty = True
+    cursession().store_objects(ds2)
     
+    # Plotting only the last MAX_DATA samples
+    if len(x_data) > MAX_DATA:
+        x_data.pop(0)
+        y_data.pop(0)
+        x_data2.pop(0)
+        y_data2.pop(0)
     x_data.append(time_ref)
-    time_ref += 1
-    y_data.append(random.random()) 
+    x_data2.append(time_ref + 10.0)
+
+    y_data.append(random.random())
+    y_data2.append(random.random()) 
     time.sleep(0.1)
-        
 
